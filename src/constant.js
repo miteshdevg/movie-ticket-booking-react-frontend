0b9679a5f73f6ab65af1cc04790bdcea100d3bfd
Mitesh <miteshdevganiya177@gmail.com>
2024-12-03 12:08:57 +0000

    change API link

--- src/constant.js
+++ src/constant.js
@@ -1,4 +1,2 @@
-export default 'http://ec2-54-83-111-249.compute-1.amazonaws.com/api'
+export default 'http://ec2-34-228-60-24.compute-1.amazonaws.com/api'
 // export default 'http://localhost:5000/api'
-
-
